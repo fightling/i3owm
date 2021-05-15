ef314296da8460f584d851520997715a30b1afd9
Patrick Hoffmann <pat@thats-software.com>
2021-05-15 02:28:49 +0200

    code review
    improved code a lot and separated stuff into different functions

--- src/main.rs
+++ src/main.rs
@@ -4,95 +4,96 @@ extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 
-use chrono::prelude::*;
-use clap::{crate_version, App, Arg};
-use http::StatusCode;
+use clap::{crate_version, App, Arg, ArgMatches};
+use regex::Regex;
+use serde::{Deserialize, Serialize};
 use std::error;
-use std::sync::mpsc;
-use std::{thread, time};
 use text_io::read;
 
 mod weather;
 
-//use weather::insert_weather;
-
-// let satellite = "🛰";
-
-fn main() -> Result<(), Box<dyn error::Error>> {
-    // fetch arguments
-    let args = App::new("i3owm")
-        .version(crate_version!())
-        .about(
-            "Open Weather extension for i3status
+// get arguments form application
+fn get_args() -> ArgMatches {
+    App::new("i3owm")
+    .version(crate_version!())
+    .about(
+        "Open Weather extension for i3status
 
 Example usage in i3config:
 
-  bar {
-    status_command i3status | i3owm -p 2 -r -k <key> -c Berlin,DE -f '{icon} {temp}{temp_unit} 💧{humidity}%'
-  }
+bar {
+status_command i3status | i3owm -p 2 -r -k <key> -c Berlin,DE -f '{icon} {temp}{temp_unit} 💧{humidity}%'
+}
 
 Output would be like:
 
-    ⛅ 11°C 💧55%
+⛅ 11°C 💧55%
 
 ",
-        )
-        .author("Patrick Hoffmann")
-        .args(&[
-            Arg::new("api")
-                .about("OpenWeatherMap API key
+    )
+    .author("Patrick Hoffmann")
+    .args(&[
+        Arg::new("api")
+            .about("OpenWeatherMap API key
 (get one at https://openweathermap.org/api)")
-                .short('k')
-                .long("api-key")
-                .takes_value(true),
-            Arg::new("city")
-                .about("location city
+            .short('k')
+            .long("api-key")
+            .takes_value(true),
+        Arg::new("city")
+            .about("location city
 (city's name, comma, 2-letter country code (ISO3166))")
-                .short('c')
-                .long("city")
-                .takes_value(true)
-                .required_unless_present("city_id")
-                .conflicts_with("city_id")
-                .default_value("Berlin,DE"),
-            Arg::new("city_id")
-                .about("location city ID
+            .short('c')
+            .long("city")
+            .takes_value(true)
+            .required_unless_present("city_id")
+            .conflicts_with("city_id")
+            .default_value("Berlin,DE"),
+        Arg::new("city_id")
+            .about("location city ID
 (search your city at https://openweathermap.org/find and take ID out of the link you get)")
-                .short('i')
-                .long("city_id")
-                .takes_value(true)
-                .required_unless_present("city")
-                .conflicts_with("city"),
-            Arg::new("format")
-                .about("format string")
-                .long_about( weather::help() )
-                .short('f')
-                .long("format")
-                .default_value("{city} {icon} {current} {temp}{temp_unit} {humidity}%")
-                .takes_value(true),
-            Arg::new("position")
-                .about("position of output in JSON when wrapping i3status")
-                .short('p')
-                .long("position")
-                .takes_value(true),
-            Arg::new("lang")
-                .about("two character language code of weather descriptions
+            .short('i')
+            .long("city_id")
+            .takes_value(true)
+            .required_unless_present("city")
+            .conflicts_with("city"),
+        Arg::new("format")
+            .about("format string")
+            .long_about( weather::help() )
+            .short('f')
+            .long("format")
+            .default_value("{city} {icon} {current} {temp}{temp_unit} {humidity}%")
+            .takes_value(true),
+        Arg::new("position")
+            .about("position of output in JSON when wrapping i3status")
+            .short('p')
+            .long("position")
+            .takes_value(true),
+        Arg::new("lang")
+            .about("two character language code of weather descriptions
 (default is 'en')")
-                .short('l')
-                .long("lang")
-                .takes_value(true),
-            Arg::new("reverse")
-                .about("reverse position (from right)")
-                .short('r')
-                .long("reverse"),
-            Arg::new("units")
-                .about("use imperial units")
-                .short('u')
-                .long("units")
-                .takes_value(true)
-                .possible_values(&["metric", "imperial", "standard"])
-                .default_value("metric"),
-        ])
-        .get_matches();
+            .short('l')
+            .long("lang")
+            .takes_value(true),
+        Arg::new("reverse")
+            .about("reverse position (from right)")
+            .short('r')
+            .long("reverse"),
+        Arg::new("units")
+            .about("use imperial units")
+            .short('u')
+            .long("units")
+            .takes_value(true)
+            .possible_values(&["metric", "imperial", "standard"])
+            .default_value("metric"),
+    ])
+    .get_matches()
+}
+
+// continuously inject weather into incoming json lines from i3status and pass through
+fn main() -> Result<(), Box<dyn error::Error>> {
+    // fetch arguments
+    let args = get_args();
+    // convert arguments to rust variables
     let city = args
         .value_of("city_id")
         .unwrap_or(args.value_of("city").unwrap());
@@ -106,75 +107,93 @@ Output would be like:
         .parse::<usize>()
         .unwrap();
     let reverse = args.is_present("reverse");
+    // start our observatory via OWM
+    let owm = weather::init(city, units, lang, apikey);
     // read first two lines and ignore them
+    // TODO: this code stinks!
     let line: String = read!("{}\n");
     println!("{}", line);
     let line: String = read!("{}\n");
     println!("{}", line);
-    // start observatory
-    let weather = weather::init(city, units, lang, apikey);
+    // remeber newest weather update amd begin with offline message
+    let mut current = "[offline]".to_string();
     loop {
         let mut line: String = read!("{}\n");
-        let prefix = line.chars().next().unwrap() == ',';
-        if prefix {
+        // handle prefix comma
+        if line.chars().next().unwrap() == ',' {
             line.remove(0);
-        }
-        if prefix {
             print!(",")
         }
-        match weather::update(weather, units) {
-            Some(properties) => replace(properties,line),
+        // update current weather info if there is an update available
+        match weather::update(format, &owm, units) {
+            Some(update) => current = update,
             _ => (),
         }
-        println!("{}", line);
+        // insert current weather info and print json string or original line
+        match insert(&line, "weather", position, reverse, &current) {
+            Ok(new) => println!("{}", new),
+            _ => println!("{}", line),
+        }
     }
 }
 
-fn replace( data : HashMap<&str, String>, &String line ) {
-     #[derive(Serialize, Deserialize, Debug)]
+// insert
+fn insert(
+    line: &str,
+    name: &str,
+    position: usize,
+    reverse: bool,
+    what: &str,
+) -> Result<String, serde_json::Error> {
+    //  i3status json entry in a struct
+    #[derive(Serialize, Deserialize, Debug)]
     struct I3StatusItem {
         name: String,
         instance: Option<String>,
         markup: String,
         color: Option<String>,
         full_text: String,
-    };
-
+    }
+    // read all incoming entries
     let mut items: Vec<I3StatusItem> = serde_json::from_str(&line)?;
-
+    // insert this one
     let w: I3StatusItem = I3StatusItem {
-        full_text: result.to_string(),
+        full_text: what.to_string(),
         markup: "none".to_string(),
-        name: "weather".to_string(),
+        name: name.to_string(),
         instance: None,
         color: None,
     };
+    // insert at given position
     if reverse {
         items.insert(items.len() - 1 - position, w);
     } else {
         items.insert(position, w);
     }
+    // format output back up json string
+    return Ok(format_json(format!("{:?}", items)));
+}
 
-    let mut r = format!("{:?}", items);
-
+fn format_json(line: String) -> String {
     // FIXIT: all the following replacements are needed because I just can not deal
     // with serde_json the right way :/ PLEASE HELP!
+    let mut line = line;
 
     // remove all the 'Item' names
     // thought about using '#[serde(rename = "name")]' but could not make it work
-    r = r.replace("I3StatusItem", "");
+    line = line.replace("I3StatusItem", "");
     // remove optional values which are 'None'
     // tried '#[serde(skip_serializing_if = "Option::is_none")]' but did not work.
-    r = r.replace(", color: None", "");
-    r = r.replace(", instance: None", "");
+    line = line.replace(", color: None", "");
+    line = line.replace(", instance: None", "");
     // add quotations arround json names. can you setup serge_json doing that?
-    r = r.replace("full_text", "\"full_text\"");
-    r = r.replace("instance", "\"instance\"");
-    r = r.replace("color", "\"color\"");
-    r = r.replace("markup", "\"markup\"");
-    r = r.replace("name", "\"name\"");
+    line = line.replace("full_text", "\"full_text\"");
+    line = line.replace("instance", "\"instance\"");
+    line = line.replace("color", "\"color\"");
+    line = line.replace("markup", "\"markup\"");
+    line = line.replace("name", "\"name\"");
     // remove the 'Some()' envelop from all optional values
     let re = Regex::new(r"Some\((?P<v>[^\)]*)\)").unwrap();
 
-    return Ok(re.replace_all(&r, "$v").to_owned().to_string());
+    return re.replace_all(&line, "$v").to_owned().to_string();
 }
--- src/weather.rs
+++ src/weather.rs
@@ -1,15 +1,10 @@
-// let satellite = "🛰";
-
 extern crate chrono;
 extern crate regex;
 extern crate reqwest;
-extern crate serde;
 extern crate serde_json;
 
 use chrono::prelude::*;
 use http::StatusCode;
-use regex::Regex;
-use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
 use std::sync::mpsc;
@@ -52,23 +47,19 @@ pub fn help() -> &'static str {
     ";
 }
 
+// type of update receiver channel whic you get from init
 pub type Receiver = mpsc::Receiver<result::Result<String, String>>;
 
 // start weather fetching which will spawn a thread that signals updates from OWM in json format
 // via the returned receiver
-pub fn init(
-    city: &str,
-    units: &str,
-    lang: &str,
-    api_key: &str,
-) -> Receiver {
+pub fn init(city: &str, units: &str, lang: &str, api_key: &str) -> Receiver {
     // generate correct request URL depending on city is id or name
-    let url = match city.parse::<u64>() {
-        Ok(ok) => format!(
+    let url = match city.parse::<u64>().is_ok() {
+        true => format!(
             "https://api.openweathermap.org/data/2.5/weather?id={}&units={}&lang={}&appid={}",
             city, units, lang, api_key
         ),
-        Err(e) => format!(
+        false => format!(
             "https://api.openweathermap.org/data/2.5/weather?q={}&units={}&lang={}&appid={}",
             city, units, lang, api_key
         ),
@@ -96,6 +87,123 @@ pub fn init(
     return rx;
 }
 
+// get some weather update or None (if there is nothing new)
+pub fn update(
+    format: &str,
+    rx: &mpsc::Receiver<result::Result<String, String>>,
+    units: &str,
+) -> Option<String> {
+    match rx.try_recv() {
+        Ok(response) => match response {
+            Ok(json) => match serde_json::from_str(&json) {
+                Ok(w) => {
+                    let v: Value = w;
+                    let directions = ["↑", "↗", "→", "↘", "↓", "↙", "←", "↖", "↑"];
+                    let mut data = HashMap::new();
+                    data.insert("{update}", Local::now().format("%H:%M").to_string());
+                    data.insert("{city}", v["name"].to_string());
+                    data.insert("{main}", v["weather"][0]["main"].to_string());
+                    data.insert("{description}", v["weather"][0]["description"].to_string());
+                    data.insert(
+                        "{icon}",
+                        icon(v["weather"][0]["icon"].as_str().unwrap()).to_string(),
+                    );
+                    data.insert("{pressure}", v["main"]["pressure"].to_string());
+                    data.insert("{humidity}", v["main"]["humidity"].to_string());
+                    data.insert("{wind}", v["wind"]["deg"].to_string());
+                    data.insert(
+                        "{wind_icon}",
+                        directions[(&v["wind"]["deg"].as_f64().unwrap() / 45.0).round() as usize]
+                            .to_string(),
+                    );
+                    data.insert(
+                        "{wind_speed}",
+                        v["wind"]["speed"].as_f64().unwrap().round().to_string(),
+                    );
+                    data.insert("{visibility}", v["visibility"].to_string());
+                    data.insert(
+                        "{visibility_km}",
+                        (v["visibility"].as_i64().unwrap() / 1000).to_string(),
+                    );
+                    data.insert(
+                        "{rain.1h}",
+                        match v["rain"]["rain.1h"].as_i64() {
+                            Some(v) => v,
+                            None => 0i64,
+                        }
+                        .to_string(),
+                    );
+                    data.insert(
+                        "{rain.3h}",
+                        match v["rain"]["rain.3h"].as_i64() {
+                            Some(v) => v,
+                            None => 0i64,
+                        }
+                        .to_string(),
+                    );
+                    data.insert(
+                        "{snow.1h}",
+                        match v["snow"]["snow.1h"].as_i64() {
+                            Some(v) => v,
+                            None => 0i64,
+                        }
+                        .to_string(),
+                    );
+                    data.insert(
+                        "{snow.3h}",
+                        match v["snow"]["snow.3h"].as_i64() {
+                            Some(v) => v,
+                            None => 0i64,
+                        }
+                        .to_string(),
+                    );
+                    data.insert(
+                        "{temp_min}",
+                        v["main"]["temp_min"].as_f64().unwrap().round().to_string(),
+                    );
+                    data.insert(
+                        "{temp_max}",
+                        v["main"]["temp_max"].as_f64().unwrap().round().to_string(),
+                    );
+                    data.insert(
+                        "{feels_like}",
+                        v["main"]["temp"].as_f64().unwrap().round().to_string(),
+                    );
+                    data.insert(
+                        "{temp}",
+                        v["main"]["temp"].as_f64().unwrap().round().to_string(),
+                    );
+                    data.insert(
+                        "{temp_unit}",
+                        match units {
+                            "standard" => "K",
+                            "metric" => "°C",
+                            "imperial" => "°F",
+                            _ => "",
+                        }
+                        .to_string(),
+                    );
+                    data.insert(
+                        "{speed_unit}",
+                        match units {
+                            "standard" => "m/s",
+                            "metric" => "m/s",
+                            "imperial" => "mi/h",
+                            _ => "",
+                        }
+                        .to_string(),
+                    );
+                    return Some(formatter(format, &data));
+                }
+                Err(_e) => return None,
+            },
+            Err(_e) => return None,
+        },
+        Err(_e) => return None,
+    }
+}
+
+// get a unicode symbol that matches the OWM icon
 fn icon(icon_id: &str) -> &str {
     let icons: HashMap<&str, &str> = [
         ("01d", "🌞"),
@@ -120,116 +228,13 @@ fn icon(icon_id: &str) -> &str {
     .iter()
     .cloned()
     .collect();
-    return icons.get(icon_id).unwrap();
+    return icons.get(icon_id).unwrap_or(&"🚫");
 }
 
-pub fn update(
-    rx: mpsc::Receiver<result::Result<String, String>>,
-    units: &str,
-) -> Option<HashMap<&str, String>> {
-    match rx.try_recv() {
-        Ok(response) => match serde_json::from_str(&response.unwrap()) {
-            Ok(x) => {
-                let v: Value = x;
-                let directions = ["↑", "↗", "→", "↘", "↓", "↙", "←", "↖", "↑"];
-                let mut data = HashMap::new();
-                data.insert("{update}", Local::now().format("%H:%M").to_string());
-                data.insert("{city}", v["name"].to_string());
-                data.insert("{main}", v["weather"][0]["main"].to_string());
-                data.insert("{description}", v["weather"][0]["description"].to_string());
-                data.insert(
-                    "{icon}",
-                    icon(v["weather"][0]["icon"].as_str().unwrap()).to_string(),
-                );
-                data.insert("{pressure}", v["main"]["pressure"].to_string());
-                data.insert("{humidity}", v["main"]["humidity"].to_string());
-                data.insert("{wind}", v["wind"]["deg"].to_string());
-                data.insert(
-                    "{wind_icon}",
-                    directions[(&v["wind"]["deg"].as_f64().unwrap() / 45.0).round() as usize]
-                        .to_string(),
-                );
-                data.insert(
-                    "{wind_speed}",
-                    v["wind"]["speed"].as_f64().unwrap().round().to_string(),
-                );
-                data.insert("{visibility}", v["visibility"].to_string());
-                data.insert(
-                    "{visibility_km}",
-                    (v["visibility"].as_i64().unwrap() / 1000).to_string(),
-                );
-                data.insert(
-                    "{rain.1h}",
-                    match v["rain"]["rain.1h"].as_i64() {
-                        Some(v) => v,
-                        None => 0i64,
-                    }
-                    .to_string(),
-                );
-                data.insert(
-                    "{rain.3h}",
-                    match v["rain"]["rain.3h"].as_i64() {
-                        Some(v) => v,
-                        None => 0i64,
-                    }
-                    .to_string(),
-                );
-                data.insert(
-                    "{snow.1h}",
-                    match v["snow"]["snow.1h"].as_i64() {
-                        Some(v) => v,
-                        None => 0i64,
-                    }
-                    .to_string(),
-                );
-                data.insert(
-                    "{snow.3h}",
-                    match v["snow"]["snow.3h"].as_i64() {
-                        Some(v) => v,
-                        None => 0i64,
-                    }
-                    .to_string(),
-                );
-                data.insert(
-                    "{temp_min}",
-                    v["main"]["temp_min"].as_f64().unwrap().round().to_string(),
-                );
-                data.insert(
-                    "{temp_max}",
-                    v["main"]["temp_max"].as_f64().unwrap().round().to_string(),
-                );
-                data.insert(
-                    "{feels_like}",
-                    v["main"]["temp"].as_f64().unwrap().round().to_string(),
-                );
-                data.insert(
-                    "{temp}",
-                    v["main"]["temp"].as_f64().unwrap().round().to_string(),
-                );
-                data.insert(
-                    "{temp_unit}",
-                    match units {
-                        "standard" => "K",
-                        "metric" => "°C",
-                        "imperial" => "°F",
-                        _ => "",
-                    }
-                    .to_string(),
-                );
-                data.insert(
-                    "{speed_unit}",
-                    match units {
-                        "standard" => "m/s",
-                        "metric" => "m/s",
-                        "imperial" => "mi/h",
-                        _ => "",
-                    }
-                    .to_string(),
-                );
-                return Some(data);
-            }
-            Err(e) => return None,
-        },
-        Err(e) => return None,
+fn formatter(format: &str, data: &HashMap<&str, String>) -> String {
+    let mut result = format.to_string();
+    for (k, v) in data {
+        result = result.replace(k, v);
     }
+    return result;
 }
